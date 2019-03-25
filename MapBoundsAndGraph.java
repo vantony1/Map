3bbdfb1428826c9ef32ae9410b488316b535a357
Victor Antony <32144387+vantony1@users.noreply.github.com>
2019-03-25 11:19:26 -0400

    Update MapBoundsAndGraph.java

--- MapBoundsAndGraph.java
+++ MapBoundsAndGraph.java
@@ -1,15 +1,3 @@
-/*Name: Victor Nikhil Antony
-CSC 172
-NetID: vantony
-ClassID: 134
-Assignment # HW10
-TR 4:50-6:05
-TA Name: Sifan Ye
-I did not collaboarated with Matthew Sundsberg on this project
-
-Class Written By: Matthew Sundsberg
- */
-
 
 // Easy holder for a graph and its dimensions without messing too much with Victor's code
 class MapBoundsAndGraph {
